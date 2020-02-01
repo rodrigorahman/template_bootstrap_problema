4c0368fa511cf690f9abc4d83ee91128a46581c9
Rodrigo Rahman <rodrigorahman@griks.com.b>
2020-02-01 06:41:14 -0300

    template funcionando

---
+++ src/app_boot.js
@@ -0,0 +1,69 @@
+/**
+ * Theme: Metrica - Responsive Bootstrap 4 Admin Dashboard
+ * Author: Mannatthemes
+ * Module/App: Main Js
+ */
+import jQuery from 'jquery';
+
+const $ = jQuery;
+
+export default class Boot {
+    
+    initSlimscroll() {
+        $('.slimscroll').slimscroll({
+            height: 'auto',
+            position: 'right',
+            size: "7px",
+            color: '#e6eaf5',
+            opacity: 1,
+            wheelStep: 5,
+            touchScrollStep: 50
+        });
+    }
+
+   
+    initEnlarge() {
+        if ($(window).width() < 1025) {
+            $('body').addClass('enlarge-menu');
+        } else {
+            if ($('body').data('keep-enlarged') != true)
+                $('body').removeClass('enlarge-menu');
+        }
+    }
+
+
+    initMainIconMenu() {
+        $(".navigation-menu a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().parent().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().parent().parent().parent().addClass("active"); // add active class to an anchor
+            }
+        });
+    }
+
+    initTopbarMenu() {
+        $('.navbar-toggle').on('click', function (event) {
+            $(this).toggleClass('open');
+            $('#navigation').slideToggle(400);
+        });
+
+        $('.navigation-menu>li').slice(-2).addClass('last-elements');
+
+        $('.navigation-menu li.has-submenu a[href="#"]').on('click', function (e) {
+            if ($(window).width() < 992) {
+                e.preventDefault();
+                $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
+            }
+        });
+    }
+    
+
+    init() {
+        this.initSlimscroll();
+        this.initEnlarge();
+        this.initMainIconMenu();
+        this.initTopbarMenu();
+    }
+}
